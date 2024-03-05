492711a1dca8ae904c57d9fc0d963cdc31a92061
Tasso Marcel <tassomarcel87@gmail.com>
2024-03-05 10:31:17 -0300

    movendo as peças

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,11 +1,11 @@
 package boardgame;
 
 public class Board {
-	
+
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
-	
+
 	public Board(int rows, int columns) {
 		if (rows < 1 || columns < 1) {
 			throw new BoardException("Deve haver ao menos 1 linha e coluna");
@@ -22,22 +22,22 @@ public class Board {
 	public int getClumns() {
 		return columns;
 	}
-	
+
 	public Piece piece(int row, int column) {
 		if (!positionExists(row, column)) {
 			throw new BoardException("não existe esta posição");
 		}
 		return pieces[row][column];
-		
+
 	}
-	
+
 	public Piece piece(Position position) {
 		if (!positionExists(position)) {
 			throw new BoardException("não existe esta posição");
 		}
 		return pieces[position.getRow()][position.getClumn()];
 	}
-	
+
 	public void placePiece(Piece piece, Position position) {
 		if (thereIsAPiece(position)) {
 			throw new BoardException("existem peças na posição: " + position);
@@ -45,20 +45,33 @@ public class Board {
 		pieces[position.getRow()][position.getClumn()] = piece;
 		piece.position = position;
 	}
-	
-	private boolean positionExists(int row, int column) {
-		return row >= 0 && row < rows && column >=0 && column < columns;
+
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getClumn()] = null;
+		return aux;
 	}
-	
+
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+
 	public boolean positionExists(Position position) {
 		return positionExists(position.getRow(), position.getClumn());
 	}
-	
+
 	public boolean thereIsAPiece(Position position) {
 		if (!positionExists(position)) {
 			throw new BoardException("não existe esta posição");
 		}
 		return piece(position) != null;
 	}
-	
+
 }
--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,12 +1,11 @@
 package chess;
 
-import java.util.Arrays;
+import boardgame.BoardException;
 
-public class ChessException extends RuntimeException{
+public class ChessException extends BoardException{
 	private static final long serialVersionUID = 1L;
 
 	public ChessException(String msg) {
 		super(msg);
 	}
-	
 }
